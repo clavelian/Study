82ed626699a297ba832ac61c9b1292b047f14782
Ian Clavel <ianclavel1991@gmail.com>
2024-08-16 02:20:00 +0800

    feat: light and dark theme

--- study/app/page.tsx
+++ study/app/page.tsx
@@ -1,7 +1,17 @@
+import ModeToggle from "@/components/lnd";
+
+ModeToggle;
 export default function Home() {
   return (
-    <main>
-      <p>HELLO WORLD</p>
-    </main>
+    <>
+      <nav>
+        <div className="absolute top-4 right-4">
+          <ModeToggle />
+        </div>
+      </nav>
+      <main>
+        <p>HELLO WORLD</p>
+      </main>
+    </>
   );
 }
