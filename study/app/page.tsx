a9d7f6115ac5b83785cfe82aae1b4e64d759ee1b
Ian Clavel <ianclavel1991@gmail.com>
2024-08-18 23:28:18 +0800

    installing button and function

--- study/app/page.tsx
+++ study/app/page.tsx
@@ -1,9 +1,11 @@
 import ModeToggle from "@/components/lnd";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <main className="flex justify-center items-center">
+      <main className="flex justify-center items-center h-[100vh]">
         <div className="flex items-center justify-center">
           <nav className="absolute top-4 right-4">
             <div>
@@ -11,8 +13,10 @@ export default function Home() {
             </div>
           </nav>
         </div>
-        <div className="text-4xl">
-          Whereas disregard and contempt for human rights have resulted
+        <div className="flex content-center justify-center place-items-center">
+          <Button variant="outline" size="icon">
+            <ChevronRight className="h-10 w-10" />
+          </Button>
         </div>
       </main>
     </>
